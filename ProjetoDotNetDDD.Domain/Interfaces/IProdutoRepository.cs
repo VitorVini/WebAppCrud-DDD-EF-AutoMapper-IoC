34a321c29e37c75ef65da45c899e10dfa9ed900b
VitorVini <vitor.vinicius2907@gmail.com>
2023-06-30 03:03:17 -0300

    Começo do Projeto

---
+++ ProjetoDotNetDDD.Domain/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using ProjetoDotNetDDD.Domain.Entities;
+using System.Collections.Generic;
+
+namespace ProjetoDotNetDDD.Domain.Interfaces
+{
+    public interface IProdutoRepository : IRepositoryBase<Produto>
+    {
+        IEnumerable<Produto> BuscarPorNome(string nome);
+    }
+}
---
+++ ProjetoDotNetDDD.Infra.Data/Repositories/ClienteRepository.cs
@@ -0,0 +1,9 @@
+﻿using ProjetoDotNetDDD.Domain.Entities;
+using ProjetoDotNetDDD.Domain.Interfaces;
+
+namespace ProjetoDotNetDDD.Infra.Data.Repositories
+{
+    public class ClienteRepository : RepositoryBase<Cliente>, IClienteRepository
+    {
+    }
+}
