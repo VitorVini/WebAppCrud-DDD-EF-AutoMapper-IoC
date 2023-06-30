6d7d818b660f3fc8285e5563fc8a6bddcc67b80a
VitorVini <vitor.vinicius2907@gmail.com>
2023-06-30 17:37:23 -0300

    Camada de Serviços Completa

--- ProjetoDotNetDDD.Domain/Interfaces/Services/IClienteService.cs
+++ ProjetoDotNetDDD.Domain/Interfaces/Services/IClienteService.cs
@@ -1,10 +1,12 @@
 ﻿
 
 using ProjetoDotNetDDD.Domain.Entities;
+using System.Collections.Generic;
 
 namespace ProjetoDotNetDDD.Domain.Interfaces.Services
 {
     public interface IClienteService : IServiceBase<Cliente>
     {
+        IEnumerable<Cliente> ObterClientesEspeciais(IEnumerable<Cliente> clientes);
     }
 }
---
+++ ProjetoDotNetDDD.Domain/Services/ClienteService.cs
@@ -0,0 +1,26 @@
+﻿
+
+using ProjetoDotNetDDD.Domain.Entities;
+using ProjetoDotNetDDD.Domain.Interfaces.Repositories;
+using ProjetoDotNetDDD.Domain.Interfaces.Services;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoDotNetDDD.Domain.Services
+{
+    public class ClienteService : ServiceBase<Cliente>, IClienteService
+    {
+        private readonly IClienteRepository _clienteRepository;
+
+        public ClienteService(IClienteRepository clienteRepository)
+            : base(clienteRepository) 
+        {
+            _clienteRepository = clienteRepository;
+        }
+
+        public IEnumerable<Cliente> ObterClientesEspeciais(IEnumerable<Cliente> clientes)
+        {
+            return clientes.Where(c => c.ClienteEspecial(c));
+        }
+    }
+}
