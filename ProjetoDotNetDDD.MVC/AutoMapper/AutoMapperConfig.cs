f2c07dff2568a48759a7ab2a180760e76089e7b6
VitorVini <vitor.vinicius2907@gmail.com>
2023-06-30 03:44:32 -0300

    AutoMapper Configurado

---
+++ ProjetoDotNetDDD.MVC/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,19 @@
+﻿
+
+using AutoMapper;
+
+namespace ProjetoDotNetDDD.MVC.AutoMapper
+{
+    public class AutoMapperConfig
+    {
+
+        public static void RegisterMappings()
+        {
+            Mapper.Initialize(x =>
+            {
+                x.AddProfile<DomainToViewModelMappingProfile>();
+                x.AddProfile<ViewModelToDomainMappingProfile>();
+            });
+        }
+    }
+}
---
+++ ProjetoDotNetDDD.MVC/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿
+
+using AutoMapper;
+using ProjetoDotNetDDD.Domain.Entities;
+using ProjetoDotNetDDD.MVC.ViewModels;
+
+namespace ProjetoDotNetDDD.MVC.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "ViewModelToDomainMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<ClienteViewModel, Cliente>();
+            Mapper.CreateMap<ProdutoViewModel, Produto>();
+        }
+    }
+}
